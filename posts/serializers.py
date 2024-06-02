6df87ed31a42ed1f1e7d569ba5c24164be215e50
Kyle S <kmsartoris@gmail.com>
2024-06-02 10:07:22 -0600

    added viewsets and routers, permsissions on user view set

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,12 @@
 # posts/serializers.py
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    
     class Meta:
         fields = (
             "id",
@@ -14,3 +16,10 @@ class PostSerializer(serializers.ModelSerializer):
             "created_at",
         )
         model = Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username")
--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,44 @@
 # posts/views.py
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from .models import Post
 from .permissions import ISAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
-
-class PostList(generics.ListCreateAPIView):
-    permission_classes = (ISAuthorOrReadOnly,)
-    queryset =  Post.objects.all()
-    serializer_class = PostSerializer
-    
-    
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#Model view set has both list and detail view
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (ISAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    
+    
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    
+    
+    
+# Without view sets
+# class PostList(generics.ListCreateAPIView):
+#     permission_classes = (ISAuthorOrReadOnly,)
+#     queryset =  Post.objects.all()
+#     serializer_class = PostSerializer
+    
+    
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (ISAuthorOrReadOnly,)
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+    
+
+# class UserList(generics.ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+    
+    
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
